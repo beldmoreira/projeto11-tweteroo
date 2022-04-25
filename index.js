9213d586adcbe3db955043d2e4404146209f54c8
Isabela <isabela_cmoreira@hotmail.com>
2022-04-25 15:40:08 -0300

    Tweets post requests

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(cors());
 app.use(express.json())
 
 const users = []
-
+const tweets = [];
 
 app.post("/sign-up", (req, res) => {
 
@@ -16,6 +16,13 @@ app.post("/sign-up", (req, res) => {
   res.send("OK");
 });
 
+app.post("/tweets", (req, res) => {
+    const {username, tweet} = req.body;
+    if (!username || !tweet) return res.sendStatus(422);
+    const user = users.find(user => user.username===username);
+    tweets.unshift({username, tweet, avatar: user.avatar});
+    res.send("OK");
+});
 
 
 app.listen(5000);
