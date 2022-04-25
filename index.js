3073c2b94c03a125d8acca24b04470d2c90f2795
Isabela <isabela_cmoreira@hotmail.com>
2022-04-25 15:14:32 -0300

    Sign up  session first commit

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
+app.use(express.json())
 
+const users = []
+
+
+app.post("/sign-up", (req, res) => {
+
+  const user = req.body;
+  users.push(user)
+
+  res.send("OK");
+});
 
 
 
